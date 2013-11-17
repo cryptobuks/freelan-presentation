35057d93a1f2b408151833ee6460e9ed90b098c5
Julien Kauffmann <julien.kauffmann@freelan.org>
2013-11-17 03:03:02 +0100

    Added more configurations

--- demo/alice/bootstrap.sh
+++ demo/alice/bootstrap.sh
@@ -8,16 +8,20 @@ apt-get -y upgrade
 grep "freelan" || echo "deb http://debian.freelan.org/ wheezy-backports main" | tee -a /etc/apt/sources.list
 wget -q http://debian.freelan.org/debian.freelan.org.key -O- | apt-key add -
 
-# Copy the configuration file
+# Copy the certificate files
 mkdir -p /etc/freelan
-cp /vagrant_config/alice.cfg /etc/freelan/freelan.cfg
-cp /vagrant_config/ca /etc/freelan/
+cp /vagrant_config/ca.crt /etc/freelan/
 cp /vagrant_config/alice.crt /etc/freelan/
 cp /vagrant_config/alice.key /etc/freelan/
+cp /vagrant_config/bob.crt /etc/freelan/
+cp /vagrant_config/chris.crt /etc/freelan/
 
 # Install freelan
 apt-get update
 apt-get -y install freelan
+
+# Copy the configuration file
+cp /vagrant_config/alice.conf /etc/freelan/freelan.conf
 
 # Enable the configuration
 sed -i 's/^#CONFIGURATIONS/CONFIGURATIONS/g' /etc/default/freelan
