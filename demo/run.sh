8c566257db8c86ba153572342338201799c0a11d
Julien Kauffmann <julien.kauffmann@freelan.org>
2013-11-18 17:37:42 +0100

    Made run script executable

--- demo/run.sh
+++ demo/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd config; git co .; git clean -f -x -d; ./gen_certs.sh; git co .; cd ..
+cd config; git checkout .; git clean -f -x -d; ./gen_certs.sh; git checkout .; cd ..
 cd alice; vagrant destroy; git clean -f -x -d; vagrant up; cd ..
 cd bob; vagrant destroy; git clean -f -x -d; vagrant up; cd ..
 cd carole; vagrant destroy; git clean -f -x -d; vagrant up; cd ..
